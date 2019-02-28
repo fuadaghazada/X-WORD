32cf3b5252ba1458e6f4761dcea1fcf22f65f0e5
Çağatay Sel <selcagatay1@hotmail.com>
2019-02-28 12:20:32 +0300

    Complete solutions

--- HW1/AIHW1.java
+++ HW1/AIHW1.java
@@ -1,39 +1,141 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package aihw1;
 
 import java.util.ArrayList;
+import java.util.Queue;
+import java.util.LinkedList;
+
 /**
  *
  * @author cagatay.sel-ug
  */
 public class AIHW1 {
 
+    public static int GOAL_LITER_1 = 1;
+    public static int GOAL_LITER_3 = 3;
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+
+        System.out.println("-----------QUESTION 1-------------------");
+        System.out.println("Goal: Reach "+GOAL_LITER_1 + " liter in a jug");
+        System.out.println("Left jug water capacity: "+State.LEFT_JUG_CAP);
+        System.out.println("Right jug water capacity: "+State.RIGHT_JUG_CAP);
+        System.out.println();
+        depthFirstSolve(GOAL_LITER_1);
         
-        ArrayList<State> goalStates = new ArrayList<State>();
-        goalStates.add(new State(0))
         
-        
-        State initial = new State(0,0);
-        ArrayList<State> visitedStates = new ArrayList<State> ();
+        System.out.println("-----------QUESTION 2-------------------");
+        System.out.println("Goal: Reach "+GOAL_LITER_3 + " liter in a jug");
+        System.out.println("Left jug water capacity: "+State.LEFT_JUG_CAP);
+        System.out.println("Right jug water capacity: "+State.RIGHT_JUG_CAP);
+        System.out.println();
+        breadthFirstSolve(GOAL_LITER_3);
+
+    }
+
+    ////////////Question 1 - Depth First Search for goal of 3 liter.///////////////////
+    public static void depthFirstSolve(int goalLiter) {
+
+        State initial = new State(0, 0, null, State.CREATION_INITIAL);
+        //Keeping track of visited states to prevent looping
+        ArrayList<State> visitedStates = new ArrayList<State>();
+        State goal = depthFirstSearch(initial, goalLiter, visitedStates);
+
+        if (goal == null) {
+            System.out.println("Goal is unreachable");
+        } else {
+            System.out.println("Goal reached");
+            goal.printPath();
+            goal = null;
+
+        }
+
+    }
+
+    ////////////Question 2 - Breadth First Search for goal of 1 liter.///////////////////
+
+    public static void breadthFirstSolve(int goalLiter) {
+
+        //Creating initial state
+        State initial = new State(0, 0, null, State.CREATION_INITIAL);
+
+        State goal = breadthFirstSearch(initial, goalLiter);
+
+        if (goal == null) {
+            System.out.println("Goal is unreachable");
+        } else {
+            System.out.println("Goal reached");
+            goal.printPath();
+            goal = null;
+
+        }
+    }
+
+    /**
+     * This method uses depth first search algorithm to find goal liter.
+     * @param cur current state
+     * @param goalLiter goal
+     * @param visitedStates list of visited states
+     * @return null or goal state
+     */
+    public static State depthFirstSearch(State cur, int goalLiter, ArrayList<State> visitedStates) {
+
+        if (cur.isGoalState(goalLiter)) {
+            return cur;
+        }
+
+        //Generating next states
+        cur.generateNextStates(visitedStates);
+        ArrayList<State> nextStates = cur.getNextStates();
+        for (int i = 0; i < nextStates.size(); i++) {
+            State goal = depthFirstSearch(nextStates.get(i), goalLiter, visitedStates);
+            if (goal != null) {
+                return goal;
+            }
+        }
+        return null;
+
+    }
+
+    /**
+     * This method uses breadth first search to solve jug problem.
+     *
+     * @param initial Initial state
+     * @param goalLitre Goal liter that is searched
+     * @return goal state or null
+     */
+    public static State breadthFirstSearch(State initial, int goalLiter) {
+
+        Queue<State> queue = new LinkedList<>();
+        //Keeping track of visited states to prevent looping
+        ArrayList<State> visitedStates = new ArrayList<State>();
         visitedStates.add(initial);
-       
-        initial.generateNextStates(visitedStates);
-        //System.out.println(visitedStates);
-        System.out.println(initial);
+
+        queue.add(initial);
+
+        while (!queue.isEmpty()) {
+            State cur = queue.remove();
+
+            //Return if the current state is goal state
+            if (cur.isGoalState(goalLiter)) {
+                /* Printing visited nodes for debugging */
+                /*System.out.println("---Visited State----");
+                 System.out.println(cur);*/
+                return cur;
+            }
+
+            //Generate next states
+            cur.generateNextStates(visitedStates);
+            ArrayList<State> nextStates = cur.getNextStates();
+
+            //Add next states to queue
+            for (int i = 0; i < nextStates.size(); i++) {
+                queue.add(nextStates.get(i));
+            }
+        }
+        return null;
     }
-    
-    public static void breadthFirstSearch(State initial){
-        
-        
-        
-    }
-    
+
 }
--- HW1/State.java
+++ HW1/State.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package aihw1;
 
 /**
@@ -11,48 +6,67 @@ package aihw1;
  */
 import java.util.ArrayList;
 
+
+
 public class State{
 	
+        //Problem constans
 	public static final int LEFT_JUG_CAP = 7;	
-	public static final int RIGHT_JUG_CAP = 5;	
-
-
+	public static final int RIGHT_JUG_CAP = 5;
+        
+        public static final String CREATION_INITIAL = "Initial";
+        public static final String CREATION_FILL_LEFT = "Filled left jug";
+        public static final String CREATION_FILL_RIGHT = "Filled right jug";
+        public static final String CREATION_EMPTY_LEFT = "Emptied left jug";
+        public static final String CREATION_EMPTY_RIGHT = "Emptied right jug";
+        public static final String CREATION_POUR_LEFT_TO_RIGHT = "Poured left to right";
+        public static final String CREATION_POUR_RIGHT_TO_LEFT = "Poured right to left";          
+        
+        
+        //Instance variables
 	private int leftJugWater;
 	private int rightJugWater;
-        private ArrayList<State> nextStates;
+        private String creation;
+        private ArrayList<State> nextStates;        
+        //To backtrack from goal state to initial state
+        private State parent;
 	
-	public State(int leftJugWater,int rightJugWater){
+	public State(int leftJugWater,int rightJugWater,State parent,String creation){
 	
 		this.leftJugWater = leftJugWater;
 		this.rightJugWater = rightJugWater;	
-                nextStates = new ArrayList<State>();
+                this.parent = parent;
+                this.creation = creation;
+                this.nextStates = new ArrayList<State>();
+                
 		
 	}
 	
 	public State fillLeftJug(){
 		int leftJugWater = LEFT_JUG_CAP;
 		int rightJugWater = this.rightJugWater;
-		State newState = new State(leftJugWater,rightJugWater);
+		State newState = new State(leftJugWater,rightJugWater,this,CREATION_FILL_LEFT);
 		return newState;
+                
 	}
 	
 	public State fillRightJug(){
 		int rightJugWater = RIGHT_JUG_CAP;
 		int leftJugWater = this.leftJugWater;
-		State newState = new State(leftJugWater,rightJugWater);
+		State newState = new State(leftJugWater,rightJugWater,this, CREATION_FILL_RIGHT);
 		return newState;
 	}
 	
 	public State emptyLeftJug(){
 		int leftJugWater=0;
 		int rightJugWater = this.rightJugWater;
-		State newState = new State(leftJugWater,rightJugWater);
+		State newState = new State(leftJugWater,rightJugWater,this, CREATION_EMPTY_LEFT );
 		return newState;
 	}
 	public State emptyRightJug(){
-		rightJugWater=0;
+		int rightJugWater=0;
 		int leftJugWater = this.leftJugWater;
-		State newState = new State(leftJugWater,rightJugWater);
+		State newState = new State(leftJugWater,rightJugWater,this,CREATION_EMPTY_RIGHT);
 		return newState;
 	}
 	
@@ -61,7 +75,7 @@ public class State{
 	*/
 	public State pourLeftToRight(){
 		
-		int totalWater = leftJugWater + rightJugWater;
+		int totalWater = this.leftJugWater + this.rightJugWater;
 		int leftJugWater = -1;
 		int rightJugWater = -1;
 		
@@ -73,7 +87,7 @@ public class State{
 			rightJugWater = RIGHT_JUG_CAP;
 			leftJugWater = totalWater-rightJugWater;
 		}	
-		State newState = new State(leftJugWater,rightJugWater);
+		State newState = new State(leftJugWater,rightJugWater,this,CREATION_POUR_LEFT_TO_RIGHT );
 		return newState;
                
 		
@@ -84,19 +98,41 @@ public class State{
 	*/
 	public State pourRightToLeft(){
 		
-		int totalWater = leftJugWater + rightJugWater;
+		int totalWater = this.leftJugWater + this.rightJugWater;
+                int leftJugWater = -1;
+		int rightJugWater = -1;
 		
 		//Pour all water from left to right and it does not exceed
-		if(totalWater<LEFT_JUG_CAP){
+		if(totalWater<=LEFT_JUG_CAP){
 			leftJugWater = totalWater;
 			rightJugWater = 0;
 		}else if(totalWater>LEFT_JUG_CAP){
 			leftJugWater = LEFT_JUG_CAP;
 			rightJugWater = totalWater-leftJugWater;
 		}
-                State newState = new State(leftJugWater,rightJugWater);
+                State newState = new State(leftJugWater,rightJugWater,this,CREATION_POUR_RIGHT_TO_LEFT);
 		return newState;
 	}
+        
+        /**
+         * This method back tracks states until initial state and reverses the order in order to print a path from root to leaf.
+         */
+        public void printPath(){
+            State cur = this;
+            ArrayList<State> path = new ArrayList<State>();
+            while(cur!=null){
+                path.add(cur);
+                cur = cur.parent;
+            }
+            
+            //Reverse printing the array
+            for(int i=path.size()-1;i>=0;i--){
+                cur = path.get(i);
+                System.out.println("Step "+(path.size()-i ) +" : " + cur.creation);
+                System.out.println(cur);                
+            }
+            
+        }
 	
 	/**
 	*This method generates possible states by applying the 6 operations defines above
@@ -133,12 +169,14 @@ public class State{
                 if(!visitedStates.contains(newState)){
                     this.nextStates.add(newState);
                     visitedStates.add(newState);
+                    
                 }
                 
 		newState = pourRightToLeft();	
                 if(!visitedStates.contains(newState)){
                     this.nextStates.add(newState);
                     visitedStates.add(newState);
+                    
                 }		
 	}
         
@@ -146,18 +184,27 @@ public class State{
         public String toString(){
             
             String str =  "Left jug water: "+ this.leftJugWater + " Right Jug Water: "+ this.rightJugWater+ "\n";
-            str +="------Next States-----"+"\n";
+            /*str +="------Next States-----"+"\n";
             for(int i=0;i<nextStates.size();i++){
                 State cur = nextStates.get(i);
                 str += "Left jug water: "+cur.leftJugWater+
                         " Right jug water: " + cur.rightJugWater + "\n";
-            }                   
+            }  */                 
             return str;
             
         }
         
         public ArrayList<State> getNextStates(){
             return nextStates;
+        }
+        
+        public boolean isGoalState(int goal){
+            
+            if(leftJugWater == goal || rightJugWater == goal){
+                return true;
+            }else{
+                return false;
+            }
         }
         
         
